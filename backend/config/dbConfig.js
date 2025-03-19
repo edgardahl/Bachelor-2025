3178a61508bfc8ed7a7492661151dddd2e9adb71
Edgar Dahl <edgardahl123@gmail.com>
2025-03-19 15:33:59 +0100

    dotenv import in dbconfig

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,6 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const DB_TYPE = process.env.DB_TYPE || "mongo";
-export const MONGO_URI =
-  process.env.MONGO_URI ||
-  "mongodb+srv://simen:8MN1jrgty4KfUXym@cluster0.pxl3w.mongodb.net/platform";
-export const POSTGRES_URI =
-  process.env.POSTGRES_URI || "postgres://user:pass@localhost:5432/myapp";
+export const MONGO_URI = process.env.MONGO_URI;
+export const POSTGRES_URI = process.env.POSTGRES_URI;
