13843b06712b05e209b7ccd058f9ef2376d8dd1f
simen <111754546+simeel@users.noreply.github.com>
2025-04-01 18:15:28 +0200

    småfiks

--- backend/create_token/jwt-token-emplyee.js
+++ backend/create_token/jwt-token-emplyee.js
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-// For user: Simen Elvhaug
+// For user: Jørgen Nonstad (employee)
 const token = jwt.sign(
   { userId: "50de70e9-9385-403c-b77c-9d2c37bb8d5b", role: "employee" }, // Ensure role is included
-  "dkiwx3n2ehl4tcu1ir0k119pe6qpjf7w",
+  process.env.JWT_SECRET,
   { expiresIn: "1h" }
 );
 
