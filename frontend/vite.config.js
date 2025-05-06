71e2b9ae257c529f86cef2ef51dd32e70dd55443
simen <111754546+simeel@users.noreply.github.com>
2025-05-06 20:10:05 +0200

    jeg liker ingenting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ let mkcert;
 let useHttps = false;
 
 if (isDev) {
+  console.log("dev mode", isDev, process.env.NODE_ENV);
   try {
     mkcert = require("vite-plugin-mkcert").default;
     require("child_process").execSync("mkcert -version", {
@@ -28,6 +29,22 @@ if (isDev) {
 }
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+
   plugins: [react(), ...(isDev && mkcert ? [mkcert()] : [])],
   server: {
     https: isDev
@@ -36,6 +53,7 @@ export default defineConfig({
           cert: "../pem/localhost.pem",
         }
       : false,
+    port: 5002,
     proxy: {
       "/api": {
         target: "https://localhost:5001",
@@ -46,6 +64,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    sourcemap: true,
   },
   define: {
     __API_BASE__: JSON.stringify(isDev ? "/api" : process.env.VITE_API_URL),
