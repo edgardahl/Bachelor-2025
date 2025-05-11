1d39965a7f6a2b50f1dc84b09480aea686156da5
simen <111754546+simeel@users.noreply.github.com>
2025-05-11 19:14:09 +0200

    endret admin filnavn og imports, la til npm run clean i package for å resette installs

--- frontend/src/pages/Admin/NewStorePage/NewStorePages.jsx
+++ frontend/src/pages/Admin/NewStorePage/NewStorePages.jsx
@@ -4,7 +4,7 @@ import axios from "../../../api/axiosInstance";
 import { toast } from "react-toastify";
 import BackButton from "../../../components/BackButton/BackButton";
 import Select from "react-select";
-import "./NewStorePage.css";
+import "./NewStorePages.css";
 
 export default function NewStorePage() {
   const navigate = useNavigate();
@@ -55,7 +55,7 @@ export default function NewStorePage() {
         toast.error("Kunne ikke hente butikksjefer.");
       }
     };
-    
+
     fetchManagers();
     fetchMunicipalities();
   }, []);
@@ -149,7 +149,9 @@ export default function NewStorePage() {
       <BackButton onClick={() => navigate(-1)} />
       <h2 className="form-title">Ny butikk</h2>
 
-      <label>Butikknavn (kun navn, f.eks. <b>"Alnabru"</b>, ikke "Obs Alnabru")</label>
+      <label>
+        Butikknavn (kun navn, f.eks. <b>"Alnabru"</b>, ikke "Obs Alnabru")
+      </label>
       <input
         name="store_name"
         ref={fieldRefs.store_name}
