783b28bfb2e333bac35d1ba1404ac89695b0c3f3
edgardahl <edgardahl123@gmail.com>
2025-05-15 03:51:25 +0200

    css filename change

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -6,12 +6,11 @@ import { FaClipboardUser } from "react-icons/fa6";
 import ButikkCard from "../../components/Cards/ButikkCard/ButikkCard";
 import ShiftCard from "../../components/Cards/ShiftCard/ShiftCard";
 import ButikkansattCard from "../../components/Cards/ButikkansattCard/ButikkansattCard";
-import "./LandingPage.css";
 import "../../components/Cards/ButikkCard/ButikkCard.css";
 import "../../components/Cards/ShiftCard/ShiftCard.css";
 import "../../components/Cards/ButikkansattCard/ButikkansattCard.css";
 
-import "./Landing.css";
+import "./landing.css";
 
 const Landing = () => {
   // Henter navigasjonsfunksjon for å rute brukeren til andre sider
